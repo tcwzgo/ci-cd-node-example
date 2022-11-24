42069b299e1c6a6d73d593aeb297aef52b7bad73
krisztian <hegyikrisztian1@gmail.com>
2022-11-24 13:10:56 +0100

    intentionally fucking up my code just to check if ci cd works

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/test", (_req, res) =>  {
-  res.status(200).send("Hello world")
+  res.status(200).send("Hllo world")
 })
 
 module.exports = app;
